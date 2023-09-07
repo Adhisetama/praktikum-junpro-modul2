d5e22f7a6dff28b3b85863c2896bac0553ad4b47
Adhisetama <adhisetama@gmail.com>
2023-09-07 14:46:54 +0700

    error handling

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace praktikum_junpro_modul2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Masukkan sebuah angka");
+                int angka = int.Parse(Console.ReadLine());
+                Console.WriteLine("Anda memasukkan : " + angka.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
